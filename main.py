2973df16d05e9c6a80b0ca5e600723023e6aa0a1
Wahyu Haryanto <way3haryanto@gmail.com>
2023-05-13 13:09:48 +0700

    update sample rate

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)  # Replace '/dev/ttyUSB0' with your Ar
 
 # Initialize FluidSynth
 fs = fluidsynth.Synth()
-settings = fs.settings()
+# Get the settings object
+settings = fs.get_settings()
 
 # Set the sample rate to match your audio system
-settings.setnum("synth.sample-rate", 48000)
+settings["synth.sample-rate"] = 48000
 
 fs.start()
 sfid = fs.sfload('./korg_x5_drums.sf2')  # Replace '/path/to/your/soundfont.sf2' with the actual path to your SoundFont file
